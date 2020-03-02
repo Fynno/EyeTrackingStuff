fcff51e6bd60b04ab8a334399d9cea763e1dd7c6
MarcoW123 <59894700+MarcoW123@users.noreply.github.com>
2020-03-02 11:14:20 +0100

    Add files via upload
    added the logic by which the Offset from the original eye is tracked for each case apart from 0 circles/no eyes tracked

--- Matlab Eyetracker/EyeTrackerCodes/Eye_Tracker.m
+++ Matlab Eyetracker/EyeTrackerCodes/Eye_Tracker.m
@@ -1,6 +1,10 @@
 clear cam
 % set webcam
 cam = webcam;
+% standard eye value (for when the eye is looking precisely straight)
+% in actual practice, these values would have to be measured precisely
+standard_left = 98.5;
+standard_right = 240.5;
 % set amount of taken frames
 exposure_time = 5;
 % link: https://de.mathworks.com/matlabcentral/answers/322849-eye-tracking-algorithm-help
@@ -11,8 +15,6 @@ exposure_time = 5;
 % detect the largest surface (aiming to detect the face)
 kern = [1 2 1; 0 0 0; -1 -2 -1];
 detector = vision.CascadeObjectDetector(); %Create a detector for face using Viola-Jones
-detector1 = vision.CascadeObjectDetector('LeftEye'); % Create detector for a single eye
-detector1.MergeThreshold = 200;
 for j = 1:1:exposure_time
     mirroredimg = snapshot(cam);  %Get a snapshot of webcam
     img = flip(mirroredimg, 2); %Flips the image horizontally
@@ -37,7 +39,43 @@ for j = 1:1:exposure_time
     edgeimg = uint8((e > 100) * 240);
     % detect circles within the "edgeimg"
     % 'Sensitivity' may need to be adjusted based on ambient light
-    [centers,radii] = imfindcircles(edgeimg,[8 18],'Sensitivity',0.93);
+    [centers,radii] = imfindcircles(edgeimg,[10 18],'Sensitivity',0.93);
+    % calculate the relative position of the eye in comparison to its
+    % normal position
+    if size(centers,1) == 2
+        % the matrix "centers" has 2 rows, which ideally means, that both eyes have been
+        % tracked.
+        value = centers(1,1);
+        if value <= 150
+            offset = value - standard_left;
+        else
+            offset = value - standard_right;
+        end
+    elseif size(centers,1) == 1
+        % the matrix "centers" has 1 row, which ideally means, that either the left or
+        % the right eye have been tracked .
+        if centers(1,1) <= 150
+            % left eye has been tracked
+            value = centers(1,1);
+            offset = value - standard_left;
+        else
+            % right eye has been tracked
+            value = centers(1,1);
+            offset = value - standard_right;
+        end
+    elseif size(centers,1) == 0;
+        % the matrix "centers" has 0 rows
+        offset = 0;
+    else
+        % the matrix "centers" has more than 2 rows. the algorithm now
+        % searches for the x-coordinate of a circle, that is closest to
+        % standard_left.
+        for j = size(centers,1)
+            [~,~,idx] = unique(round(abs(centers(j,1) - standard_left)),'stable');
+            value = centers(idx==1);
+        end
+    end
+    % calculate
     imagesc(edgeimg);
     % End of copied part.
     % display image
